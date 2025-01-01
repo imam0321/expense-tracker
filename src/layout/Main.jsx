d58e8ae5fd2cc07ed8c39ffd6d90925958bc66ea
imam0321 <imam.hossain0321@gmail.com>
2025-01-01 17:18:21 +0600

    project setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "../components/Navbar/Navbar";
+import { Home } from "../components/Home/Home/Home";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+};
+
+export default Main;
